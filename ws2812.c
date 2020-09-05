658479c831aa86d82e08a40cd6297fd312a89655
David Chen <david87124@gmail.com>
2020-09-05 19:27:52 +0800

    ws2812訊號改用SPI傳輸

--- ws2812.c
+++ ws2812.c
@@ -3,13 +3,11 @@
 #include "ws2812.h"
 
 // Data Type Definitions
-TM_TimeBaseInitTypeDef 	TM_TimeBaseInitStructure;
-TM_OutputInitTypeDef 		TM_OutputInitStructure;
-PDMACH_InitTypeDef 			PDMACH_InitStructure;
-HT_PDMA_TypeDef 				PDMA_TEST_Struct;
+SPI_InitTypeDef         SPI_InitStructure;
+PDMACH_InitTypeDef      PDMACH_InitStructure;
+HT_PDMA_TypeDef         PDMA_TEST_Struct;
 
-u32 WS2812[WS_PIXEL][WS_24BITS];
-//static u8 ws_i;
+u8 WS2812[WS_PIXEL + 3][WS_24BITS];
 
 bool didSetColor;
 
@@ -17,20 +15,20 @@ void wsInit(void) {
 	u8 i = 0, j = 0;
 	wsCKCUConfig();
 	wsAFIOConfig();
-	wsPWMConfig();
-	wsNVICConfig();
-	MCTM_CHMOECmd(HT_MCTM0, ENABLE);
+	wsPDMAConfig();
+	wsSPIConfig();
 	for(i = 0; i < WS_PIXEL; i++) {
 		for(j = 0; j < WS_24BITS; j++) {
 			WS2812[i][j] = WS_LOGIC_0;	
 		}
 	}
-//	for(i = 0; i < WS_24BITS; i++) {
-//		for (j = 0; j < 3; j++) {
-//			WS2812[WS_PIXEL	+ j][i] = 0;
-//		}
-//	}
+	for(i = 0; i < WS_24BITS; i++) {
+		WS2812[WS_PIXEL	+ 0][i] = 0;
+		WS2812[WS_PIXEL	+ 1][i] = 0;
+		WS2812[WS_PIXEL	+ 2][i] = 0;
+	}
 	wsClearAll();
+	wsShow();
 }
 
 void wsCKCUConfig(void) {
@@ -38,54 +36,62 @@ void wsCKCUConfig(void) {
 		CKCU_PeripClockConfig_TypeDef CKCUClock = {{ 0 }};
 		CKCUClock.Bit.PB = 1;
 		CKCUClock.Bit.PDMA = 1;
-		CKCUClock.Bit.MCTM0 = 1;
+		CKCUClock.Bit.SPI0 = 1;
 		CKCU_PeripClockConfig(CKCUClock, ENABLE);
 	}
 }
 
 void wsAFIOConfig(void) {
-	AFIO_GPxConfig(GPIO_PB, AFIO_PIN_0, AFIO_MODE_4); 	//Port B -> Pin  0  >>>  TM_CH_1
-}
-
-void wsPWMConfig(void) {
-	TM_TimeBaseInitStructure.CounterReload = WS_RELOAD;
-	TM_TimeBaseInitStructure.Prescaler = WS_PRESCALER;
-	TM_TimeBaseInitStructure.RepetitionCounter = 0;
-	TM_TimeBaseInitStructure.CounterMode = TM_CNT_MODE_UP;
-	TM_TimeBaseInitStructure.PSCReloadTime = TM_PSC_RLD_IMMEDIATE;
-	TM_TimeBaseInit(HT_MCTM0, &TM_TimeBaseInitStructure);
-	TM_ClearFlag(HT_MCTM0, TM_FLAG_UEV);
-	TM_Cmd(HT_MCTM0, ENABLE);
-	
-	TM_OutputInitStructure.Channel = TM_CH_1;
-	TM_OutputInitStructure.OutputMode = TM_OM_PWM1;
-	TM_OutputInitStructure.Control = TM_CHCTL_ENABLE;
-	TM_OutputInitStructure.ControlN = TM_CHCTL_DISABLE;
-	TM_OutputInitStructure.Polarity = TM_CHP_NONINVERTED;
-	TM_OutputInitStructure.PolarityN = TM_CHP_NONINVERTED;
-	TM_OutputInitStructure.IdleState = MCTM_OIS_LOW;
-	TM_OutputInitStructure.IdleStateN = MCTM_OIS_HIGH;
-	TM_OutputInitStructure.Compare = 0;
-	TM_OutputInit(HT_MCTM0, &TM_OutputInitStructure);
-	
-	TM_Cmd(HT_MCTM0, ENABLE);
+	AFIO_GPxConfig(GPIO_PB, AFIO_PIN_2, AFIO_FUN_SPI);
+  AFIO_GPxConfig(GPIO_PB, AFIO_PIN_3, AFIO_FUN_SPI);
+  AFIO_GPxConfig(GPIO_PB, AFIO_PIN_4 , AFIO_FUN_SPI);
+  AFIO_GPxConfig(GPIO_PB, AFIO_PIN_5 , AFIO_FUN_SPI);
 }
 
 void wsPDMAConfig(void) {
-	PDMACH_InitStructure.PDMACH_SrcAddr = (u32)WS2812;
-	PDMACH_InitStructure.PDMACH_DstAddr = (u32)&(HT_MCTM0 -> CH1CCR);
-	PDMACH_InitStructure.PDMACH_BlkCnt = WS_BLOCK;
-	PDMACH_InitStructure.PDMACH_BlkLen = 1;
-	PDMACH_InitStructure.PDMACH_DataSize = WIDTH_32BIT;
-	PDMACH_InitStructure.PDMACH_Priority = M_PRIO;
-	PDMACH_InitStructure.PDMACH_AdrMod = SRC_ADR_LIN_INC | DST_ADR_FIX | AUTO_RELOAD;
-	PDMA_Config(PDMA_CH5, &PDMACH_InitStructure);
-	
-	PDMA_IntConfig(PDMA_CH5, PDMA_INT_TC, ENABLE);
+  /* Master Master Tx PDMA channel configuration                                                              */
+  PDMACH_InitStructure.PDMACH_SrcAddr = (u32) &WS2812;
+  PDMACH_InitStructure.PDMACH_DstAddr = (u32) &HT_SPI0->DR;
+  PDMACH_InitStructure.PDMACH_BlkCnt = WS_BLOCK;
+  PDMACH_InitStructure.PDMACH_BlkLen = 1;
+  PDMACH_InitStructure.PDMACH_DataSize = WIDTH_8BIT;
+  PDMACH_InitStructure.PDMACH_Priority = VH_PRIO;
+  PDMACH_InitStructure.PDMACH_AdrMod = SRC_ADR_LIN_INC | DST_ADR_FIX;
+  PDMA_Config(PDMA_CH1, &PDMACH_InitStructure);
+
+  /* Enable Master Tx, Rx GE & TC interrupt                                                                   */
+  PDMA_IntConfig(PDMA_CH1, PDMA_INT_GE | PDMA_INT_TC, ENABLE);
+
+  /* Enable the corresponding PDMA channel                                                                  */
+  PDMA_EnaCmd(PDMA_CH1, ENABLE);
 }
 
-void wsNVICConfig(void) {
-	NVIC_EnableIRQ(PDMACH2_5_IRQn);	// Enable PDMA Channel 2 to 5 interrupt
+void wsSPIConfig(void) {
+  /* MASTER_SEL idle state is HIGH                                                                            */
+  GPIO_PullResistorConfig(HT_GPIOB, AFIO_PIN_2, GPIO_PR_UP);
+
+  /* Master configuration                                                                                     */
+  SPI_InitStructure.SPI_Mode = SPI_MASTER;
+  SPI_InitStructure.SPI_FIFO = SPI_FIFO_DISABLE;
+  SPI_InitStructure.SPI_DataLength = SPI_DATALENGTH_8;
+  SPI_InitStructure.SPI_SELMode = SPI_SEL_HARDWARE;
+  SPI_InitStructure.SPI_SELPolarity = SPI_SELPOLARITY_LOW;
+  SPI_InitStructure.SPI_CPOL = SPI_CPOL_LOW;
+  SPI_InitStructure.SPI_CPHA = SPI_CPHA_FIRST;
+  SPI_InitStructure.SPI_FirstBit = SPI_FIRSTBIT_MSB;
+  SPI_InitStructure.SPI_RxFIFOTriggerLevel = 0;
+  SPI_InitStructure.SPI_TxFIFOTriggerLevel = 0;
+  SPI_InitStructure.SPI_ClockPrescaler = WS_PRESCALER;
+  SPI_Init(HT_SPI0, &SPI_InitStructure);
+
+  /* Enable Master & Slave                                                                                     */
+  SPI_Cmd(HT_SPI0, ENABLE);
+
+  /* Set SS as output mode for slave select                                                                 */
+  SPI_SELOutputCmd(HT_SPI0, ENABLE);
+
+  /* Enable master Master PDMA requests later                                                                 */
+  SPI_PDMACmd(HT_SPI0, SPI_PDMAREQ_TX | SPI_PDMAREQ_RX, ENABLE);
 }
 
 void wsSetColor(u8 pixelNum, u8 color[], float mag) {
@@ -109,15 +115,8 @@ void wsSetColor(u8 pixelNum, u8 color[], float mag) {
 
 void wsShow(void) {
 	wsPDMAConfig();
-	
-	TM_PDMAConfig(HT_MCTM0, TM_PDMA_UEV, ENABLE);
-	TM_Cmd(HT_MCTM0, ENABLE);
-	PDMA_EnaCmd(PDMA_CH5, ENABLE);
-	while (PDMA_GetFlagStatus(PDMA_CH5, PDMA_FLAG_TC) == RESET);	// Wait for transferring data complete
-	PDMA_ClearFlag(PDMA_CH5, PDMA_FLAG_TC);							// Clear PDMA_FLAG_TC of PDMA_CH5 before transferring data
-	TM_PDMAConfig(HT_MCTM0, TM_PDMA_UEV, DISABLE);
-	TM_Cmd(HT_MCTM0, DISABLE);
-	PDMA_EnaCmd(PDMA_CH5, DISABLE);
+	wsSPIConfig();
+	while (!PDMA_GetFlagStatus(PDMA_CH1, PDMA_FLAG_TC));
 }
 
 void wsClearAll(void) {
@@ -126,20 +125,17 @@ void wsClearAll(void) {
 	for(i = 0; i < WS_PIXEL; i++) wsSetColor(i, ws_clear, 0);
 }
 
-void wsClearBetween(u8 startPixelNum, u8 endPixelNum) {
-	u8 i = 0;
-	u8 ws_clear[3] = {0};
-	for(i = startPixelNum; i <= endPixelNum; i++) wsSetColor(i, ws_clear, 1);
-}
-
 void wsBlinkAll(u32 wait) {
 	u8 i = 0;
 	u8 ws_white[3] = {255, 255, 255};
-	wait *= 10000;
-
-	for(i = 0; i < WS_PIXEL; i++) wsSetColor(i, ws_white, 0.1);
-	wsShow();
-	while(wait--);
+	u32 w;
+	for(i = 0; i < WS_PIXEL; i++) {
+		w = wait * 10000;
+		wsClearAll();
+		wsSetColor(i, ws_white, 0.1);
+		wsShow();
+		while(w--);
+	}
 	wsClearAll();
 	wsShow();
 }
--- ws2812.h
+++ ws2812.h
@@ -1,57 +1,21 @@
 #include "ht32.h"
 
-#define WS_RELOAD			(60)													// (48000000/800000)	
-#define WS_PRESCALER	0
-#define WS_PIXEL			128 														
-#define WS_LOGIC_1		38														// High Bit Duty
-#define WS_LOGIC_0		19														// Low Bit Duty
-#define	WS_24BITS			24														// Number of Bits of each LED
-#define WS_BLOCK			(WS_PIXEL * WS_24BITS)	// PDMA transfer blocks
+#define WS_PRESCALER  7
+#define WS_PIXEL      8 														
+#define WS_LOGIC_1    0xF8                            // High Bit Duty
+#define WS_LOGIC_0    0xC0                            // Low Bit Duty
+#define WS_24BITS     24                              // Number of Bits of each LED
+#define WS_BLOCK      ((WS_PIXEL + 3) * WS_24BITS)    // PDMA transfer blocks
                                                  
-#define WS_LEV_SIZE   8
-#define WS_FRQ_SIZE   16
-
-//extern u8 ws_green[];
-//extern u8 ws_blue[];
+#define WS_LEV_SIZE   16
+#define WS_FRQ_SIZE   9
 
 void wsInit(void);
-
 void wsCKCUConfig(void);
-
 void wsAFIOConfig(void);
-
-void wsPWMConfig(void);
-
 void wsPDMAConfig(void);
-
-void wsNVICConfig(void);
-
+void wsSPIConfig(void);
 void wsSetColor(u8 pixelNum, u8 color[], float mag);
-
 void wsShow(void);
-
 void wsClearAll(void);
-
-void wsSetRow(u8 row[], u8 color[]);
-
-void wsSetCol(u8 col[], u8 color[]);
-
-void wsClearBetween(u8 startPixelNum, u8 endPixelNum);
-
 void wsBlinkAll(u32 wait);
-
-void wsPrintBuffer(void);
-
-void wsSetLeftArrow(u8 color[]);
-
-void wsSetRightArrow(u8 color[]);
-
-void wsSetNumber(u8 number);
-
-void wsSetMusicStatus(bool play, u8 color[]);
-
-void wsSetTransAnim(u8 *pictureCount);
-
-void wsSetScoreAnim(u8 *pictrueCount);
-
-void wsSetModeLed(u8 mode);
